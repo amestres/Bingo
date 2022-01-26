6b742b2b534bd33112e189f9f6b682949e8f367b
Adrià Mestres <amestreszk@gmail.com>
2022-01-26 21:49:28 +0100

    rellenar arrays

--- js/main.js
+++ js/main.js
@@ -1,2 +1,34 @@
-const hola = "holaaa"
-console.log(hola);
+const arrayNumeros = Array(90)
+const arrayJugador = Array(15)
+const arrayCPU = Array(15)
+
+rellenarBombo()
+createCard(arrayJugador)
+createCard(arrayCPU)
+
+function rellenarBombo (){
+    for(let i = 1; i <= arrayNumeros.length; i++){
+        arrayNumeros[i-1] = i;
+    }
+    console.log(arrayNumeros)
+}
+
+function createCard(array){
+    let numero = 0;
+    let index = 0;
+    for(let x = 0; x <= array.length-1; x++){
+        numero = Math.floor(Math.random() * (90 - 1 ) + 1)
+        index = array.indexOf(numero)
+        while(index != -1){
+            numero = Math.floor(Math.random() * (91 - 1 ) + 1)
+            index = array.indexOf(numero)
+        }
+        array[x] = numero
+    }
+    array.sort(function(a,b){return a-b;})
+    console.log(array)
+}
+
+
+
+
